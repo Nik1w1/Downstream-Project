b6d9ec7d0263936afc2fabbec9905d1ed917a2bc
Nik1w1 <nikita.a.puranik@gmail.com>
2025-08-21 15:10:02 +0200

    Created Player controller
    Started working, added pseudocode

---
+++ DownstreamProj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int StepsTaken;
+    private bool FootOn = True; //Which foot the player should take next step with True = Right, False = Left
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StepsTaken = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.ArrowUp))
+        {
+            StepsTaken++;
+            Debug.Log("Steps taken: " + StepsTaken);
+            //Add code to move the player forward
+            //Add code to run animations
+            if (FootOn)
+            {
+                Debug.Log("Right foot forward");
+            }
+            else
+            {
+                Debug.Log("Left foot forward");
+            }
+        }
+    }
+
+}
